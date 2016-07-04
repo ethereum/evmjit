82b96c11e055eb2de25a98fb6f277f62424b2fa9
Paweł Bylica <chfast@gmail.com>
2016-07-05 00:54:51 +0200

    EVM-C: Register selfdestruction by evm_update()
    This allows simplifying the return structure.

--- libevmjit/evmjit.cpp
+++ libevmjit/evmjit.cpp
@@ -3,7 +3,7 @@
 static_assert(sizeof(evm_uint256) == 32, "evm_uint256 is too big");
 static_assert(sizeof(evm_hash256) == 32, "evm_hash256 is too big");
 static_assert(sizeof(evm_hash160) == 20, "evm_hash160 is too big");
-static_assert(sizeof(evm_result) == 32 + 8, "evm_result is too big");
+static_assert(sizeof(evm_result) == 32, "evm_result is too big");
 static_assert(sizeof(evm_query_key) == sizeof(int), "Non-standard enum size");
 
 #ifndef _MSC_VER
