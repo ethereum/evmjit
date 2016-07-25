4dc53c2ea09b4ed9f43509e0e3ff54a469410652
Paweł Bylica <chfast@gmail.com>
2016-07-25 14:55:43 +0200

    Use EVM-C to init the JIT instance
    Currently you can have only single JIT instance.

--- include/evmjit/JIT.h
+++ include/evmjit/JIT.h
@@ -187,9 +187,6 @@ public:
 class JIT
 {
 public:
-	/// This function is going to be replaced with evm_create().
-	EVMJIT_API static void init(evm_query_fn _queryFn, evm_update_fn _updateFn, evm_call_fn _callFn);
-
 	/// Ask JIT if the EVM code is ready for execution.
 	/// Returns `true` if the EVM code has been compiled and loaded into memory.
 	/// In this case the code can be executed without overhead.
