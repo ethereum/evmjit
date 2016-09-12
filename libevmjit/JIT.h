0f49cc97ae21e1c57fc52f741810c6bdd0d63a97
Paweł Bylica <chfast@gmail.com>
2016-09-12 12:49:35 +0200

    Remove unused fields of RuntimeData

--- libevmjit/JIT.h
+++ libevmjit/JIT.h
@@ -32,16 +32,7 @@ struct RuntimeData
 		GasPrice,
 		CallData,
 		CallDataSize,
-		Address,
-		Caller,
-		Origin,
-		TransferredCallValue,	// actual ether amount transferred
 		ApparentCallValue,		// value of msg.value - different during DELEGATECALL
-		CoinBase,
-		Difficulty,
-		GasLimit,
-		Number,
-		Timestamp,
 		Code,
 		CodeSize,
 
@@ -55,16 +46,7 @@ struct RuntimeData
 	int64_t 	gasPrice = 0;
 	byte const* callData = nullptr;
 	uint64_t 	callDataSize = 0;
-	i256 		address;
-	i256 		caller;
-	i256 		origin;
-	i256 		transferredValue;
 	i256 		apparentValue;
-	i256 		coinBase;
-	i256 		difficulty;
-	i256 		gasLimit;
-	uint64_t 	number = 0;
-	int64_t 	timestamp = 0;
 	byte const* code = nullptr;
 	uint64_t 	codeSize = 0;
 };
